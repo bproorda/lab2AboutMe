4d69dcba93a7e7a441317a7e20c1f3e0b1e87f78
bproorda <bproorda@gmail.com>
2020-02-27 08:57:35 -0600

    fixed a couple errors and tried to add another function, not working yet

--- js/app.js
+++ js/app.js
@@ -24,27 +24,31 @@ var points = 0
 var quesArray = ['Was I born in Boston?', 'Do I have two Dogs?', 'Do I drive a Dodge Demon?', 'Is my favorite novel genre fantasy/sci-fi?', 'Do I have a complete pair of lungs'];
 var quizAns = ['n', 'y', 'n', 'y', 'n'];
 var correctRes = ['That is Correct! Good Job!', 'That is Correct! For now...', 'That is Correct...sadly...', 'That is Correct! Maud\'dib is pleased', 'That is Correct! ...don\'t ask'];
-var wrongAns = ['Nope! Nice try though.', 'Nope! Though I would not mind a third one...', 'I wish...', 'Nope! Gandalf is not happy', 'Nope! if only...'];
-var elseAns = ['Invalid Answer Detected!', 'Invalid Answer Detected!', 'Invalid Answer Detected!', 'Invalid Answer Detected!', 'Invalid Answer Detected!'];
-var quizArray = [quesArray, quizAns, correctRes, wrongAns, elseAns];
-var j = 0;
+var wrongRes = ['Nope! Nice try though.', 'Nope! Though I would not mind a third one...', 'I wish...', 'Nope! Gandalf is not happy', 'Nope! if only...'];
+var wrongAns = ['y', 'n', 'y', 'n', 'y'];
+var quizArray = [quesArray, quizAns, correctRes, wrongRes, wrongAns];
+var quizBool = false;
 
 for(var i = 0; i < 5; i++) {
-    var ans1 = prompt(quizArray[0][i]);
-    console.log(ans1);
-    var lans1 = ans1.toLowerCase();
-    console.log(lans1)
-    if (lans1 === quizArray[1][i]) {
-    console.log("Visitor is correct")
-    alert(quizArray[2][i]);
-    points++
-    } else if (lans1 !== quizArray[1][i]) {
-    alert(quizArray[3][i]);
-    console.log("Visitor is incorrect")
-    } else {
-    alert("Invalid Answer Detected!");
-    console.log("Visitor is incorrect")
-}
+    // while (quizBool === false) {
+        var ans1 = prompt(quizArray[0][i]);
+        console.log(ans1);
+        var lans1 = ans1.toLowerCase();
+        console.log(lans1)
+        if (lans1 === quizArray[1][i]) {
+            console.log("Visitor is correct")
+            alert(quizArray[2][i]);
+            quizBool = true;
+            points++
+        } else if (lans1 === quizArray[4][i]) {
+            alert(quizArray[3][i]);
+            console.log("Visitor is incorrect")
+            quizBool = true;
+        } else {
+            alert("Invalid Answer Detected!");
+            console.log("Visitor is incorrect")
+    }
+// }
 }
 //guess the number
 //got help from w3 for random integer
@@ -80,7 +84,7 @@ var musicTypes = ['country', 'classical', 'christian'];
 var guess2;
 var musicBool = false;
 for (var count2 = 0; count2 < 6; count2++) {
-    guess2 = prompt('Can you guess one of the genres of music I like?').toLocaleLowerCase();
+    guess2 = prompt('Can you guess one of the genres of music I like?').toLowerCase();
     console.log(guess2);
     for (var y = 0; y < 3; y++) {
         if (guess2 === musicTypes[y]) {
